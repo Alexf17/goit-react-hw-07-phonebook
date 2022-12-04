143b76c4d1c8f4f706285cd59ae799d368019db1
Oleksii <achili3f17@gmail.com>
2022-12-04 22:20:10 +0200

    update style

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -4,7 +4,7 @@ export const TitleBook = styled.h2`
   color: black;
   display: flex;
   justify-content: center;
-  align-contacts: center;
+  align-items: center;
   justify-content: center;
   font-size: 30px;
   margin-bottom: 15px;
@@ -14,14 +14,14 @@ export const Wrap = styled.div`
   padding: 60px 300px;
   display: flex;
   flex-direction: column;
-  align-contacts: center;
+  align-items: center;
 `;
 
 export const ContactTitle = styled.h2`
   color: #252526;
   display: flex;
   justify-content: center;
-  align-contacts: center;
+  align-items: center;
   justify-content: center;
   font-size: 35px;
   margin-bottom: 15px;
